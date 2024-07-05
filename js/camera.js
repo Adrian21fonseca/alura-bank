3c088dbb013d933fe93dfdfcb11ca589d9429bc8
Adrian Fonseca <adrian2020fm@gmail.com>
2024-07-05 13:50:42 -0500

    first commit

--- js/camera.js
+++ js/camera.js
@@ -1,5 +1,3 @@
-
-
 const botonAbrirCamara = document.querySelector("[data-video-boton]")
 const video = document.querySelector("[data-video]")
 const campoCamara = document.querySelector("[data-camera]")
@@ -8,22 +6,35 @@ const botonTomarFoto = document.querySelector("[data-tomar-foto]")
 const mensaje = document.querySelector("[data-mensaje]")
 const canvas = document.querySelector("[data-video-canvas]")
 
+const botonEnviar = document.querySelector("[data-enviar]")
+
 let imgUrl = ""
 
 botonAbrirCamara.addEventListener("click", async ()=>{
-    const iniciarVideo = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    const iniciarVideo = await navigator.mediaDevices.getUserMedia({
+        video:true, 
+        audio:false,
+    });
 
     botonAbrirCamara.style.display = "none";
     campoCamara.style.display = "block";
-    video.srcObject = iniciarVideo
-})
+    video.srcObject = iniciarVideo;
+});
 
 botonTomarFoto.addEventListener("click", ()=>{
-    canvas.getContext("2d").drawImage(video, 0, 0, canvas.Width, canvas.height);
+    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
 
     imgUrl = canvas.toDataURL("image/jpeg");
 
     campoCamara.style.display = "none";
     mensaje.style.display = "block";
 
+});
+
+botonEnviar.addEventListener("click", ()=>{
+    const recibirDatos = localStorage.getItem("registro")
+    const convertirDatos = JSON.parse(recibirDatos)
+    convertirDatos.img_url = imgUrl;
+    localStorage.setItem("registro", JSON.stringify(convertirDatos))
+    window.location.href = "./abrir-cuenta-form-3.html";
 })
